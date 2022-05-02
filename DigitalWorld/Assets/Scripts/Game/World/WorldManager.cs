bbb15e6e50edcea2a8a3355ed2fd0bfae198cad8
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-05-02 23:21:44 +0800

    移除无用的东西

--- DigitalWorld/Assets/Scripts/Game/World/WorldManager.cs
+++ DigitalWorld/Assets/Scripts/Game/World/WorldManager.cs
@@ -2,7 +2,6 @@
 using DigitalWorld.Behaviour;
 using DigitalWorld.Proto.Game;
 using DigitalWorld.Table;
-using DigitalWorld.TileMap;
 using Dream.Extension.Unity;
 using DreamEngine;
 using System.Collections.Generic;
@@ -22,16 +21,6 @@ namespace DigitalWorld.Game
             }
         }
         private Camera mainCamera = null;
-
-        /// <summary>
-        /// 地图
-        /// </summary>
-        public TileMapControl TileMapControl
-        {
-            get { return tileMapControl; }
-        }
-
-        private TileMapControl tileMapControl = null;
 
         /// <summary>
         /// 单位词典
@@ -73,73 +62,10 @@ namespace DigitalWorld.Game
         {
             unitIdPool = 0;
             this.units.Clear();
-
-            this.SetupMap();
-        }
-
-        private void SetupMap()
-        {
-            string fullPath = "Res/Map/Map.prefab";
-
-            GameObject gameObject = null;
-
-            Object target = AssetManager.LoadAsset<Object>(fullPath);
-
-            if (null != target)
-            {
-                gameObject = (GameObject)Instantiate(target);
-                if (null != gameObject)
-                {
-                    gameObject.name = target.name;
-                    Transform t = gameObject.transform;
-                    t.position = Vector3.zero;
-                }
-            }
-
-            if (null != gameObject)
-            {
-                tileMapControl = gameObject.GetOrAddComponent<TileMapControl>();
-                if (null != tileMapControl)
-                {
-                    string fileName = string.Format("{0}.bytes", 1);
-                    string path = Path.Combine(TileMapControl.defaultMapDataPath, fileName);
-
-                    TextAsset mapTA = AssetManager.LoadAsset<TextAsset>(path);
-                    MapData data = new MapData();
-                    data.Decode(mapTA.bytes, 0);
-
-                    tileMapControl.Setup(data);
-                }
-            }
         }
         #endregion
 
         #region Create & Register
-        public ControlTile CreateTile(TileData tileData)
-        {
-            TilebaseInfo info = tileData.TilebaseInfo;
-            if (null == info)
-                return null;
-
-            string path = string.Format("{0}.prefab", info.prefabPath);
-            GameObject obj = AssetManager.LoadAsset<GameObject>(path);
-            if (null == obj)
-                return null;
-
-            GameObject go = GameObject.Instantiate(obj) as GameObject;
-            ControlTile tile = ControlTile.GetOrAddControl(go, (ETileType)tileData.tileBaseId);
-            UnitData unitData = new UnitData()
-            {
-                configId = tileData.tileId,
-                unitType = EUnitType.Tile,
-                level = 1,
-            };
-
-            tile.OnBorn();
-            this.RegisterUnit(tile, unitData);
-            return tile;
-        }
-
         private uint GetNewUnitId()
         {
             return ++unitIdPool;
