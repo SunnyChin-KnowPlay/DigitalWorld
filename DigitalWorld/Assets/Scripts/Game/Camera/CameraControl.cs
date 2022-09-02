2f4e8365584c048a78029cd1ce2e7876a2480cd0
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-09-03 03:33:19 +0800

    初步实现相机功能。

--- DigitalWorld/Assets/Scripts/Game/Camera/CameraControl.cs
+++ DigitalWorld/Assets/Scripts/Game/Camera/CameraControl.cs
@@ -9,24 +9,22 @@ namespace DigitalWorld.Game
     /// </summary>
     public class CameraControl : MonoBehaviour
     {
+        #region Params
         public Transform target;
         protected Transform trans;
 
         public float distance = 15;
 
         public Vector2 distanceClamp = new Vector2(5, 25);
-        public Vector2 heightAngleClamp = new Vector2(15, 75);
+        public Vector2 verticalAngleClamp = new Vector2(0, 89);
 
-        public const float mouseScrollWheelSpeed = 1;
+        public const float mouseScrollWheelSpeed = 5;
 
-        /// <summary>
-        /// 鼠标左键的dir
-        /// </summary>
-        private Vector3 leftDir;
+
         /// <summary>
         /// 鼠标右键的相机和目标的偏移量
         /// </summary>
-        private Vector3 rightDir;
+        private Vector3 inputAngles;
 
         /// <summary>
         /// 最后一次的目标位置
@@ -36,75 +34,73 @@ namespace DigitalWorld.Game
         private Vector3 oldMousePosition;
         public float mouseMoveSpeed = 90;
 
-
         private float standardDirSqrMagnitude = 0;
         private const float standardDirSqrMagnitudeLimit = 1;
+        #endregion
+
+        #region Common
+        private Quaternion GetStandardHorizontalRotation()
+        {
+            return Quaternion.Euler(0, target.rotation.eulerAngles.y, 0);
+        }
+
+        private float ClampAngle(float angle, float min, float max)
+        {
+            return Mathf.Clamp(angle, min, max);
+        }
+        #endregion
 
         private void Start()
         {
             trans = this.transform;
-            leftDir = new Vector3(0, 10, -10) * distance;
-            rightDir = new Vector3(0, 10, -10) * distance;
+            inputAngles = new Vector3(0, 1, 1) * distance;
         }
 
         void Update()
         {
-            this.distance = Mathf.Clamp(this.distance + Input.GetAxis("Mouse ScrollWheel") * mouseScrollWheelSpeed, distanceClamp.x, distanceClamp.y);
+            float axis = Input.GetAxis("Mouse ScrollWheel");
+            this.distance = Mathf.Clamp(this.distance + axis * mouseScrollWheelSpeed, distanceClamp.x, distanceClamp.y);
+        }
 
-            float mag = (target.position - lastedTargetPosition).sqrMagnitude;
-            lastedTargetPosition = target.position;
-
-            standardDirSqrMagnitude += mag;
-
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
-            {
-                oldMousePosition = Input.mousePosition;
-            }
-
+        private void LateUpdate()
+        {
             if (null != target)
             {
+                float mag = (target.position - lastedTargetPosition).sqrMagnitude;
+                lastedTargetPosition = target.position;
+
+                standardDirSqrMagnitude += mag * 10f;
+
+                if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                {
+                    oldMousePosition = Input.mousePosition;
+                }
+
+                if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+                {
+                    Vector3 deltaPosition = Input.mousePosition - oldMousePosition;
+                    this.oldMousePosition = Input.mousePosition;
+
+                    inputAngles += mouseMoveSpeed * Time.deltaTime * deltaPosition;
+                    inputAngles = new Vector3(inputAngles.x, ClampAngle(inputAngles.y, verticalAngleClamp.x, verticalAngleClamp.y), 0);
+                }
+
                 if (Input.GetMouseButton(0))
                 {
-                    Vector3 deltaPosition = Input.mousePosition - oldMousePosition;
-                    this.oldMousePosition = Input.mousePosition;
-
-                    trans.RotateAround(target.position, target.right, mouseMoveSpeed * Time.deltaTime * deltaPosition.y);
-                    trans.RotateAround(target.position, Vector3.up, this.mouseMoveSpeed * Time.deltaTime * deltaPosition.x);
-
-                    leftDir = trans.position - target.position;
-
-                    float cos = Vector3.Dot(Vector3.up, leftDir) / Vector3.Magnitude(leftDir);
-                    if (cos <= 0 || cos >= 0.99f)
-                    {
-                        trans.RotateAround(target.position, target.right, -mouseMoveSpeed * Time.deltaTime * deltaPosition.y);
-                        leftDir = trans.position - target.position;
-                    }
-
                     standardDirSqrMagnitude = 0;
                 }
-
-                if (Input.GetMouseButton(1))
+                else if (Input.GetMouseButton(1))
                 {
-                    Vector3 deltaPosition = Input.mousePosition - oldMousePosition;
-                    this.oldMousePosition = Input.mousePosition;
-
-                    trans.RotateAround(target.position, target.right, mouseMoveSpeed * Time.deltaTime * deltaPosition.y);
-                    trans.RotateAround(target.position, Vector3.up, this.mouseMoveSpeed * Time.deltaTime * deltaPosition.x);
-
-                    leftDir = rightDir = trans.position - target.position;
-
-                    float cos = Vector3.Dot(Vector3.up, rightDir) / Vector3.Magnitude(rightDir);
-                    if (cos <= 0 || cos >= 0.99f)
-                    {
-                        trans.RotateAround(target.position, target.right, -mouseMoveSpeed * Time.deltaTime * deltaPosition.y);
-                        leftDir = rightDir = trans.position - target.position;
-                    }
+                    standardDirSqrMagnitude = standardDirSqrMagnitudeLimit;
                 }
 
+                Quaternion quaternion = Quaternion.identity;
+                Quaternion inputHorizontalRotation = Quaternion.AngleAxis(inputAngles.x, Vector3.up);
+                Quaternion inputVerticalRotation = Quaternion.AngleAxis(inputAngles.y, Vector3.right);
+                quaternion *= Quaternion.Slerp(inputHorizontalRotation, GetStandardHorizontalRotation(), standardDirSqrMagnitude / standardDirSqrMagnitudeLimit);
+                quaternion *= inputVerticalRotation;
 
-                Vector3 finalDir = Vector3.Lerp(leftDir, rightDir, standardDirSqrMagnitude / standardDirSqrMagnitudeLimit);
-                trans.position = target.position + finalDir.normalized * distance;
-
+                trans.position = target.position + quaternion * -Vector3.forward * distance;
                 trans.LookAt(target.position);
             }
         }
