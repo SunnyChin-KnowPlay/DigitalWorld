a47d1a33533902f5171c9678692d2d7af62f8462
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-09-12 22:13:01 +0800

    加入定义

---
+++ DigitalWorld/Assets/Defined/Scripts/Generated/Game.cs
@@ -0,0 +1,37 @@
+using Dream.Core;
+
+namespace DigitalWorld.Defined.Game
+{
+        	/// <summary>
+    /// 属性类型
+    /// </summary>
+    public enum EPropertyType : int
+    {
+  
+        /// <summary>
+        /// 血量
+        /// </summary>
+        Hp = 1,
+  
+        /// <summary>
+        /// 攻击力
+        /// </summary>
+        Attack = 2,
+  
+        /// <summary>
+        /// 移动速度
+        /// </summary>
+        MoveSpeed = 3,
+    }
+        	/// <summary>
+    /// 单位类型
+    /// </summary>
+    public enum EUnitType : int
+    {
+  
+        /// <summary>
+        /// 角色
+        /// </summary>
+        Character = 1,
+    }
+}
