6f63b90be4426ea946c504aa2e03a5200bcb73a7
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-09-23 02:34:09 +0800

    界面动画功能，加入毛玻璃动效

---
+++ DigitalWorld/Assets/DreamEngine/UI/Editor/Elements/WidgetPanelEditor.cs
@@ -0,0 +1,31 @@
+﻿using DreamEngine.UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace DreamEditor.UI
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(WidgetPanel))]
+    public class WidgetPanelEditor : Editor
+    {
+        private WidgetPanel panelTarget;
+
+        private void OnEnable()
+        {
+            panelTarget = (WidgetPanel)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            panelTarget.animationFunction = (EPanelSwitchAnimationFunction)EditorGUILayout.EnumFlagsField("Animation Functions", panelTarget.animationFunction);
+
+            // 保存上面Toggle设置值
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(target);
+            }
+        }
+    }
+}
--- DigitalWorld/Assets/DreamEngine/UI/Scripts/Elements/WidgetPanel.cs
+++ DigitalWorld/Assets/DreamEngine/UI/Scripts/Elements/WidgetPanel.cs
@@ -5,16 +5,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DreamEngine.UI
 {
+    /// <summary>
+    /// 面板开关动画类型
+    /// </summary>
+    public enum EPanelSwitchAnimationFunction
+    {
+        /// <summary>
+        /// 空
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 透明度
+        /// </summary>
+        Alpha = 1 << 0,
+        /// <summary>
+        /// 模糊
+        /// </summary>
+        Blur = 1 << 1,
+    }
+
     public class WidgetPanel : Widget
     {
         #region Params
         private RectTransform root;
 
+        /// <summary>
+        /// 开关动画曲线
+        /// </summary>
+        public AnimationCurve alphaAnimationCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+        /// <summary>
+        /// 开关动画曲线
+        /// </summary>
+        public AnimationCurve blurAnimationCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 4));
+
+        /// <summary>
+        /// 开场动画耗时
+        /// </summary>
         public float showAniamtionDuration = 0.3f;
+        /// <summary>
+        /// 退场动画耗时
+        /// </summary>
         public float hideAniamtionDuration = 0.3f;
+
+        /// <summary>
+        /// 动画功能枚举
+        /// </summary>
+        [SerializeField]
+        [HideInInspector]
+        public EPanelSwitchAnimationFunction animationFunction;
+
         #endregion
 
         #region Mono
@@ -42,6 +85,27 @@ namespace DreamEngine.UI
         }
         #endregion
 
+        #region Logic
+        /// <summary>
+        /// 检查是否包含动画功能
+        /// </summary>
+        /// <param name="function"></param>
+        /// <returns></returns>
+        private bool CheckHasAnimationFunction(EPanelSwitchAnimationFunction function)
+        {
+            return (animationFunction & function) == function;
+        }
+
+        /// <summary>
+        /// 检查是否不存在任何动画功能
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckAnimationFunctionsIsEmpty()
+        {
+            return animationFunction == EPanelSwitchAnimationFunction.None;
+        }
+        #endregion
+
         #region Animation
         private void StopSwitchCoroutines()
         {
@@ -51,33 +115,74 @@ namespace DreamEngine.UI
 
         protected virtual IEnumerator ApplyEnter()
         {
-            if (!root.TryGetComponent<CanvasGroup>(out var cg))
-                yield break;
-
-            float t = 0;
-            cg.alpha = t;
-            while (t < showAniamtionDuration)
+            if (!CheckAnimationFunctionsIsEmpty())
             {
-                t += Time.deltaTime;
-                float a = t / showAniamtionDuration;
-                cg.alpha = Mathf.Min(a, 1);
-                yield return new WaitForEndOfFrame();
+                bool hasFunctionAlpha = CheckHasAnimationFunction(EPanelSwitchAnimationFunction.Alpha);
+                hasFunctionAlpha &= root.TryGetComponent<CanvasGroup>(out CanvasGroup cg);
+
+                bool hasFunctionBlur = CheckHasAnimationFunction(EPanelSwitchAnimationFunction.Blur);
+                hasFunctionBlur &= root.TryGetComponent<Image>(out Image image);
+
+                float t = 0;
+                float speed = 1 / showAniamtionDuration;
+
+                if (hasFunctionAlpha && null != cg)
+                {
+                    cg.alpha = alphaAnimationCurve.Evaluate(t);
+                }
+
+
+                if (hasFunctionBlur && null != image)
+                {
+                    image.material.SetFloat("_Size", blurAnimationCurve.Evaluate(t));
+                }
+
+                while (t < 1)
+                {
+                    t += Time.deltaTime * speed;
+                    if (hasFunctionAlpha)
+                    {
+                        cg.alpha = Mathf.Min(alphaAnimationCurve.Evaluate(t), 1);
+                    }
+
+                    if (hasFunctionBlur)
+                    {
+                        image.material.SetFloat("_Size", blurAnimationCurve.Evaluate(t));
+                    }
+                    yield return new WaitForEndOfFrame();
+                }
             }
         }
 
         protected virtual IEnumerator ApplyExit()
         {
-            if (!root.TryGetComponent<CanvasGroup>(out var cg))
-                yield break;
-
-            float t = showAniamtionDuration;
-            
-            while (t > 0)
+            if (!CheckAnimationFunctionsIsEmpty())
             {
-                t -= Time.deltaTime;
-                float a = t / showAniamtionDuration;
-                cg.alpha = Mathf.Min(a, 1);
-                yield return new WaitForEndOfFrame();
+                bool hasFunctionAlpha = CheckHasAnimationFunction(EPanelSwitchAnimationFunction.Alpha);
+                hasFunctionAlpha &= root.TryGetComponent<CanvasGroup>(out CanvasGroup cg);
+
+                bool hasFunctionBlur = CheckHasAnimationFunction(EPanelSwitchAnimationFunction.Blur);
+                hasFunctionBlur &= root.TryGetComponent<Image>(out Image image);
+
+                float t = 1;
+                float speed = 1 / hideAniamtionDuration;
+
+                while (t > 0)
+                {
+                    t -= Time.deltaTime * speed;
+
+                    if (hasFunctionAlpha)
+                    {
+                        cg.alpha = Mathf.Min(alphaAnimationCurve.Evaluate(t), 1);
+                    }
+
+                    if (hasFunctionBlur )
+                    {
+                        image.material.SetFloat("_Size", blurAnimationCurve.Evaluate(t));
+                    }
+
+                    yield return new WaitForEndOfFrame();
+                }
             }
 
             this.gameObject.SetActive(false);
