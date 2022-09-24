7020e286bedbef60d87e9251572ef78345605edf
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-09-24 17:55:00 +0800

    加入词典

--- DigitalWorld/Assets/Scripts/Inputs/InputManager.cs
+++ DigitalWorld/Assets/Scripts/Inputs/InputManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using DreamEngine.Core;
+using System.Collections.Generic;
 
 namespace DigitalWorld.Inputs
 {
@@ -8,6 +9,11 @@ namespace DigitalWorld.Inputs
     /// </summary>
     public class InputManager : Singleton<InputManager>
     {
-
+        #region Params
+        /// <summary>
+        /// 事件和输入键位映射词典
+        /// </summary>
+        private Dictionary<EventCode, KeyCode> eventCodes = new Dictionary<EventCode, KeyCode>();
+        #endregion
     }
 }
