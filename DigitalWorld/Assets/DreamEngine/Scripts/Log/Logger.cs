fc8fb0a9b1e73a06c1c12a3b58c7210b2f6c94d8
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2022-09-26 02:30:47 +0800

    代码整理

--- DigitalWorld/Assets/DreamEngine/Scripts/Log/Logger.cs
+++ DigitalWorld/Assets/DreamEngine/Scripts/Log/Logger.cs
@@ -100,57 +100,7 @@ namespace DreamEngine.Log
             }
         }
         #endregion
-        //        #region GUI
-        //#if UNITY_EDITOR
-        //        [UnityEditor.Callbacks.OnOpenAssetAttribute(0)]
-        //        private static bool OnOpenAsset(int instanceID, int line)
-        //        {
-        //            string stackTrace = GetStackTrace();
-        //            if (!string.IsNullOrEmpty(stackTrace) && stackTrace.Contains("Logger.cs"))
-        //            {
-        //                System.Text.RegularExpressions.Match matches = System.Text.RegularExpressions.Regex.Match(stackTrace, @"\(at (.+)\)", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-        //                string pathLine = "";
-        //                while (matches.Success)
-        //                {
-        //                    pathLine = matches.Groups[1].Value;
-
-        //                    if (!pathLine.Contains("DreamEngine/Scripts/Log/Logger.cs"))
-        //                    {
-        //                        int splitIndex = pathLine.LastIndexOf(":");
-        //                        string path = pathLine.Substring(0, splitIndex);
-        //                        line = System.Convert.ToInt32(pathLine.Substring(splitIndex + 1));
-        //                        string fullPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("Assets"));
-        //                        fullPath = fullPath + path;
-        //                        UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(fullPath.Replace('/', '\\'), line);
-        //                        break;
-        //                    }
-        //                    matches = matches.NextMatch();
-        //                }
-        //                return true;
-        //            }
-        //            return false;
-        //        }
-
-        //        private static string GetStackTrace()
-        //        {
-        //            var ConsoleWindowType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.ConsoleWindow");
-        //            var fieldInfo = ConsoleWindowType.GetField("ms_ConsoleWindow", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-        //            var consoleInstance = fieldInfo.GetValue(null);
-        //            if (consoleInstance != null)
-        //            {
-        //                if ((object)UnityEditor.EditorWindow.focusedWindow == consoleInstance)
-        //                {
-        //                    fieldInfo = ConsoleWindowType.GetField("m_ActiveText", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-        //                    string activeText = fieldInfo.GetValue(consoleInstance).ToString();
-        //                    return activeText;
-        //                }
-        //            }
-        //            return null;
-        //        }
-        //#endif
-        //        #endregion
-
-
+       
 
         public static void InfoFormat(UnityEngine.Object context, string format, params object[] args)
         {
