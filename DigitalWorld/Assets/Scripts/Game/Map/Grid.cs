ea0c21c3a727b57f05da90122cbc0c37288f8a32
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2023-03-22 01:30:46 +0800

    先生成了一份，明天继续写。

--- DigitalWorld/Assets/Scripts/Game/Map/Grid.cs
+++ DigitalWorld/Assets/Scripts/Game/Map/Grid.cs
@@ -1,21 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace DigitalWorld.Game
 {
-    internal class Grid
+    public enum Direction
     {
-        public int Index => index;
-        protected int index;
-
-        public Map Map
-        {
-            get => map;
-            set => map = value;
-        }
-        protected Map map;
+        Up,
+        Left,
+        Down,
+        Right
     }
+
+    public enum GridType
+    {
+        Empty,
+        Wall,
+        Water,
+        Grass
+    }
+
+
+    public class Grid
+    {
+        public int index;
+        public Vector2Int position;
+        public GridType type;
+        public bool isWalkable;
+        public Map map;
+
+        public Grid(int index, Vector2Int position, GridType type, bool isWalkable, Map map)
+        {
+            this.index = index;
+            this.position = position;
+            this.type = type;
+            this.isWalkable = isWalkable;
+            this.map = map;
+        }
+
+        // get neighbor grid in specific direction
+        public Grid GetNeighbor(Direction direction)
+        {
+            Vector2Int neighborPosition = position;
+            switch (direction)
+            {
+                case Direction.Up:
+                    neighborPosition += Vector2Int.up;
+                    break;
+                case Direction.Left:
+                    neighborPosition += Vector2Int.left;
+                    break;
+                case Direction.Down:
+                    neighborPosition += Vector2Int.down;
+                    break;
+                case Direction.Right:
+                    neighborPosition += Vector2Int.right;
+                    break;
+            }
+            return map.GetGrid(neighborPosition);
+        }
+    }
+
+    
+    
+
+   
 }
--- DigitalWorld/Assets/Scripts/Game/Map/Map.cs
+++ DigitalWorld/Assets/Scripts/Game/Map/Map.cs
@@ -1,10 +1,40 @@
-﻿namespace DigitalWorld.Game
-{
-    /// <summary>
-    /// 地图类
-    /// </summary>
-    internal class Map
-    {
+﻿using System.Collections.Generic;
+using UnityEngine;
 
+namespace DigitalWorld.Game
+{
+    public class Map
+    {
+        public int width;
+        public int height;
+        public List<Grid> grids;
+
+        public Map(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.grids = new List<Grid>();
+
+            // create all grids
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    grids.Add(new Grid(y * width + x, new Vector2Int(x, y), GridType.Empty, true, this));
+                }
+            }
+        }
+
+        // get grid at position
+        public Grid GetGrid(Vector2Int position)
+        {
+            int index = position.y * width + position.x;
+            if (index < 0 || index >= grids.Count)
+            {
+                return null;
+            }
+            return grids[index];
+        }
     }
+
 }
