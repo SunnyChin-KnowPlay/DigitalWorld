e77a0d758bf34b2e0f548795d34e022a57d6fddf
SunnyChin-KnowPlay <sunnychin@vip.163.com>
2023-03-22 00:29:26 +0800

    格子

---
+++ DigitalWorld/Assets/Scripts/Game/Map/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalWorld.Game
+{
+    internal class Grid
+    {
+        public int Index => index;
+        protected int index;
+
+        public Map Map
+        {
+            get => map;
+            set => map = value;
+        }
+        protected Map map;
+    }
+}
